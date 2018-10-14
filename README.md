537fedee21cf0f42603d16745b9a0298251a7dde
David Robles <davidrobles@ccpaedu.com>
2018-10-14 22:30:46 +0000

    base

--- README.md
+++ README.md
@@ -21,17 +21,17 @@ In this unit, coders will create a Rock, Paper, Scissors game. The Rock, Paper, 
 
 #### Wrap
 - [x] Push your changes!
-- [ ] Save and submit your website using the link on the Daily Session Document.
+- [x] Save and submit your website using the link on the Daily Session Document.
 
 ## Day 2
 
 ### Javascript 
 
-- [ ] Outside of your click handler, declare 3 variables (userChoice, computerChoice and winner) and assign them values of empty strings
-- [ ] Outside of your click handler, declare a randomNumber variable and set it equal to 0
-- [ ] In your click handler, generate a random number and assign it to the randomNumber variable
-- [ ] In your click handler, store the input value in the userChoice variable	
-- [ ] In your click handler, display userChoice to the screen
+- [x] Outside of your click handler, declare 3 variables (userChoice, computerChoice and winner) and assign them values of empty strings
+- [x] Outside of your click handler, declare a randomNumber variable and set it equal to 0
+- [x] In your click handler, generate a random number and assign it to the randomNumber variable
+- [x] In your click handler, store the input value in the userChoice variable	
+- [x] In your click handler, display userChoice to the screen
 #### Wrap
 - [ ] Push your changes!
 
--- js/script.js
+++ js/script.js
@@ -18,11 +18,38 @@ var winner = "";
 
 
 
+
     $("#shoot").click(function()
     {
         userChoice = $("#input").val();
         computerChoice = Math.floor(Math.random()*computerChoices.length);
         $("#userChoice").text(userChoice);
         $("#computerChoice").text(computerChoices[computerChoice]);
+        $("#input").val("");
+        if(userChoice == "rock" || userChoice == "paper" || userChoice == "scissors")
+        {
+            if(userChoice == "rock" && computerChoices[computerChoice] == "scissors" || userChoice == "paper" && computerChoices[computerChoice] == "rock" || userChoice == "scissors" && computerChoices[computerChoice] == "paper")
+            {
+                winner = name;
+                $("#winner").text(winner);
+            }
+            if(userChoice == "rock" && computerChoices[computerChoice] == "paper" || userChoice == "paper" && computerChoices[computerChoice] == "scissors" || userChoice == "scissors" && computerChoices[computerChoice] == "rock")
+            {
+                winner = "Computer";
+                $("#winner").text(winner);
+            }
+            if(userChoice == "rock" && computerChoices[computerChoice] == "rock" || userChoice == "paper" && computerChoices[computerChoice] == "paper" || userChoice == "scissors" && computerChoices[computerChoice] == "scissors")
+            {
+                winner = "neither";
+                $("#winner").text(winner);
+            }
+        }
+        else
+        {
+            winner = "ERROR";
+            $("#winner").text(winner);
+            $("#userChoice").text("");
+            $("#computerChoice").text("");
+        }
     });
 
